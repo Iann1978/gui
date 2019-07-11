8b4454ea4a1017a7bad2c3281e0b5f5f45c6463f
iann <iann1978@163.com>
2019-07-11 09:52:36 +0800

    - Change texture type from GL_RGB to GL_RGBA.

--- src/gui/engine/FrameBuffer.cpp
+++ src/gui/engine/FrameBuffer.cpp
@@ -10,7 +10,7 @@ FrameBuffer::FrameBuffer()
 
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, Screen::width, Screen::height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, Screen::width, Screen::height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
