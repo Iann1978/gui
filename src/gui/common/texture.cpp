3a4f305fd065dfc6ab0d014950866e30c957fca4
iann <iann1978@163.com>
2019-07-12 05:58:14 +0800

    * Fixed bug: Wrong color when generate Gaussian Template.

--- src/gui/common/texture.cpp
+++ src/gui/common/texture.cpp
@@ -229,18 +229,18 @@ GLuint generateGaussianTemplate()
 
 	// "Bind" the newly created texture : all future texture functions will modify this texture
 	glBindTexture(GL_TEXTURE_2D, textureID);
-	unsigned char data[] = { 2,4,5,4,2,
-		4,9,12,9,4,
-		5,12,15,12,5,
-		4,9,12,9,4,
-		2,4,5,4,2,
+	unsigned char data[] = { 2,2,2,4,4,4,5,5,5,4,4,4,2,2,2,
+		4,4,4,9,9,9,12,12,12,9,9,9,4,4,4,
+		5,5,5,12,12,12,15,15,15,12,12,12,5,5,5,
+		4,4,4,9,9,9,12,12,12,9,9,9,4,4,4,
+		2,2,2,4,4,4,5,5,5,4,4,4,2,2,2,
 	};
 	for (int i = 0; i < sizeof(data); i++)
 	{
 		data[i] = (unsigned char)(1.0f * 255 * data[i] / 159);
 	}
 	// Give the image to OpenGL
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 5, 5, 0, GL_RED, GL_UNSIGNED_BYTE, data);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 5, 5, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 
 	// OpenGL has now copied the data. Free our own version
 
